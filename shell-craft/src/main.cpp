64fd0a81050fd30fc61572748d26d722a151e1fc
SANJAY J <sanjayjayakumar2006@gmail.com>
2026-09-12 15:36:43 +0530

    Delete shell-craft directory

--- shell-craft/src/main.cpp
+++
@@ -1,9 +0,0 @@
-// ShellCraft - a custom Unix shell written in C++
-// Milestone 0: project skeleton. Verifies that the build toolchain works.
-
-#include <iostream>
-
-int main() {
-    std::cout << "ShellCraft starting..." << std::endl;
-    return 0;
-}
